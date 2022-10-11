cdcf5b5bafa9f9b8887714028764c17f3c0e5369
Abduljebar <57292618+Abduljebar49@users.noreply.github.com>
2022-10-11 13:07:01 +0300

    angular init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/labetee'),
+      dir: require('path').join(__dirname, './coverage/labete-fe'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MemberComponent } from './home/member/member.component';
-import { WhlabeteComponent } from './home/whlabete/whlabete.component';
-import { TalentComponent } from './talent/talent.component';
-import { HomeComponent } from './home/home.component';
-const routes: Routes = [
-  { path: 'talent', component: TalentComponent },
-  { path: 'member', component: MemberComponent },
-  { path: 'whlabete', component: WhlabeteComponent },
-  { path: '', component: HomeComponent },
 
-];
+const routes: Routes = [];
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
 export class AppRoutingModule { }
