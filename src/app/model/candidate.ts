e808121c92002574052eb631688c3baaf1ffb279
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-10-19 21:05:14 +0300

    made a model

---
+++ src/app/model/candidate.ts
@@ -0,0 +1,42 @@
+export interface Candidate {
+  name: string;
+  bDate: string;
+  email: string;
+  address: string;
+  phone: number;
+  sex: string;
+}
+export const CANDIDATE: Candidate[] = [
+  {
+    name: 'beno',
+    bDate: 'JULY',
+    email: 'beno@gmail.com',
+    address: 'Addis ababa,ethiopia',
+    phone: 0988174455,
+    sex: 'F',
+  },
+  {
+    name: 'Abdi',
+    bDate: 'Augest',
+    email: 'abdi@gmail.com',
+    address: 'asmara,ethiopia',
+    phone: 0988174455,
+    sex: 'F',
+  },
+  {
+    name: 'naiema',
+    bDate: 'septemper',
+    email: 'nomail@gmail.com',
+    address: 'Adama,ethiopia',
+    phone: 0988174455,
+    sex: 'F',
+  },
+  {
+    name: 'MOhammed',
+    bDate: 'Januarry',
+    email: 'Mohammed@gmail.com',
+    address: 'Addis ababa,ethiopia',
+    phone: 09512423,
+    sex: 'F',
+  },
+];
---
+++ src/app/model/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  full_name: string;
+  user_ID: number;
+  BDate: Date;
+  Phone: number;
+  Email: string;
+  Sex: string;
+  role_ID: number;
+}
