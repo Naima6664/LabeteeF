0b9f1bf90ee78bffe968b9f7a6553c4c35e256af
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-10-23 10:13:46 +0300

    finished impelemting mock data

--- src/app/index/home/home.component.ts
+++ src/app/index/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidate } from 'src/app/model/candidate';
+import { CAN } from 'src/app/model/mock';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  Can: Candidate[] = CAN;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/model/mock.ts
@@ -0,0 +1,36 @@
+import { Candidate } from './candidate';
+
+export const CAN: Candidate[] = [
+  {
+    name: 'abdurahman',
+    bDate: '19jul',
+    email: 'beno@gmail.com',
+    address: 'addis ababa',
+    phone: '0988174455',
+    sex: 'Female',
+  },
+  {
+    name: 'Abduljaber',
+    bDate: '19jul',
+    email: 'beno@gmail.com',
+    address: 'addis ababa',
+    phone: '0817283783',
+    sex: 'Female',
+  },
+  {
+    name: 'NEMIAMA',
+    bDate: '19jul',
+    email: 'beno@gmail.com',
+    address: 'addis ababa',
+    phone: '09881743455',
+    sex: 'Female',
+  },
+  {
+    name: 'ABD',
+    bDate: '19jul',
+    email: 'beno@gmail.com',
+    address: 'addis ababa',
+    phone: '09881742455',
+    sex: 'Female',
+  },
+];
