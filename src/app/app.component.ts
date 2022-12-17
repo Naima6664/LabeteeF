e3609fe67cc708d98f8cba6f72e719ed5bd9ba2c
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-12-18 01:08:18 +0300

    fixed the errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'labete-fe';
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
+  form = new FormGroup({});
   login = {
     username: '',
     password: '',
@@ -22,7 +23,7 @@ export class SigninComponent implements OnInit {
         label: 'Email address',
         placeholder: 'Enter email',
         required: true,
-      }
+      },
     },
     {
       key: 'name',
@@ -31,18 +32,15 @@ export class SigninComponent implements OnInit {
         label: 'username',
         placeholder: 'Enter username',
         required: true,
-      }
-    }
+      },
+    },
   ];
 
   onSubmit(model: any) {
     console.log(model);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
