97cd6d5cc77076ac11f0d8f034ac7ea352c7e99d
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-12-18 00:54:17 +0300

    Revert "finsihed th html for formly'"
    This reverts commit f0bddd2b840c2b9735cfd012437a342cd656bb4d.

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -5,7 +5,7 @@ import { FormlyFieldConfig } from '@ngx-formly/core';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss'],
+  styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
   login = {
@@ -22,7 +22,7 @@ export class SigninComponent implements OnInit {
         label: 'Email address',
         placeholder: 'Enter email',
         required: true,
-      },
+      }
     },
     {
       key: 'name',
@@ -31,16 +31,18 @@ export class SigninComponent implements OnInit {
         label: 'username',
         placeholder: 'Enter username',
         required: true,
-      },
-    },
+      }
+    }
   ];
 
-  onSubmit() {
-    console.log();
+  onSubmit(model: any) {
+    console.log(model);
   }
-  onChange(wow: any, data: any) {}
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
 }
